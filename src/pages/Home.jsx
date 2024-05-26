0f2ceabf82ff2e9e0be096c46b1cb024f4f50974
Justin Caringal <jacfriday10th@gmail.com>
2024-05-26 01:21:25 -0500

    Set up basic layout of beginning of Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,5 @@
 import { TypeAnimation } from "react-type-animation"
+import { HomeCard } from "../components/HomeCard"
 import DigitalCodeRain from "../assets/DigitalCodeRainAnimation_OlegGamulinskii_4kUHD.gif"
 import './Home.css'
 
@@ -19,13 +20,13 @@ export function Home() {
 
   return (
     <>
-      <div className="landing">
+      <div id="landing">
         <img
           src={DigitalCodeRain}
           id="digital-code-rain"
           alt="Landing page"
         />
-        <div className="home-text-grouping">
+        <div id="home-text-grouping">
           <TypeAnimation
             sequence={typeSequence}
             wrapper="span"
@@ -35,13 +36,37 @@ export function Home() {
           />
           <br/>
           <br/>
-          <p className="landing-blurb">
+          <p id="landing-blurb">
             A multifaceted individual of eclectic tastes
             who possesses a great love of learning.
           </p>
         </div>
       </div>
-      <div style={{height: "10vh"}}/> {/* gives room for animation */}
+      <div style={{height: "15vh"}}/> {/* gives room for animation */}
+      <div style={{textAlign: "center"}}>
+        <HomeCard
+          title="This is test title 1"
+          img="https://lyndellinstitute.com/wp-content/uploads/2019/05/Justin-about.jpg"
+          side="left"
+        >
+          This is a test paragraph
+        </HomeCard>
+        <HomeCard
+          title="This is test title 2"
+          img=""
+          side="right"
+        >
+          This is a test paragraph
+        </HomeCard>
+        <HomeCard
+          title="This is test title 3"
+          img=""
+          side="left"
+        >
+          This is a test paragraph
+        </HomeCard>
+        
+      </div>
       <div style={{height: " 100vh"}}/>
     </>
   )
